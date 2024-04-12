7886038ca77ec9efab2a3fbf6945035908bc7dd1
Mathieu <mathieu.boucher@epsi.fr>
2024-04-12 12:29:48 +0200

    Add a condition to prevent multiplication of the welcome message in console

--- app/_components/WelcomeConsoleMessage.tsx
+++ app/_components/WelcomeConsoleMessage.tsx
@@ -2,11 +2,19 @@
 
 export const WelcomeConsoleMessage = () => {
     if (typeof window !== 'undefined') {
-        console.log(
-            "%cWelcome to my website !\n %cBuilt with React, Next.js, and Tailwind CSS.",
-            "color: red; font-size: 20px; font-weight: bold; text-align: center;",
-            "color: white; font-size: 16px; font-weight: bold; text-align: center;"
-        );
+
+        // Prevent the console message from showing up every time the page is refreshed
+        const hasConsoleMessageBeenShown = localStorage.getItem('consoleMessageShown');
+
+        if (hasConsoleMessageBeenShown) {
+            console.log(
+                "%cWelcome to my website !\n %cBuilt with React, Next.js, and Tailwind CSS.",
+                "color: red; font-size: 20px; font-weight: bold; text-align: center;",
+                "color: white; font-size: 16px; font-weight: bold; text-align: center;"
+            );
+        }
+
+        localStorage.setItem('consoleMessageShown', 'true');
     }
 
     return null;
