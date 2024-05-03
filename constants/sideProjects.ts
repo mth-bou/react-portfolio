f26e7288c2b6cbbf80e6228cbd0b01f34a73be24
Mathieu <mathieu.boucher@epsi.fr>
2024-05-03 18:59:22 +0200

    Add more side projects

--- constants/sideProjects.ts
+++ constants/sideProjects.ts
@@ -1,7 +1,20 @@
-import { faCartShopping, faSignal } from "@fortawesome/free-solid-svg-icons";
+import {
+    faCartShopping,
+    faSignal,
+    faDiagramProject,
+    faTableCells,
+    faRobot,
+    faPersonBiking
+} from "@fortawesome/free-solid-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 export const SIDE_PROJECTS = (dict: any) => [
+    {
+        logo: faGithub,
+        title: "Githreads",
+        description: dict.Experiences.sideProjectsCard.githreadsProject.desc,
+        url: "https://githreads-mthb.vercel.app/"
+    },
     {
         logo: faCartShopping,
         title: "Symfony Ecom App",
@@ -9,10 +22,28 @@ export const SIDE_PROJECTS = (dict: any) => [
         url: "https://github.com/mth-bou/ecom"
     },
     {
-        logo: faGithub,
-        title: "Githreads",
-        description: dict.Experiences.sideProjectsCard.githreadsProject.desc,
-        url: "https://githreads-mthb.vercel.app/"
+        logo: faDiagramProject,
+        title: "Task management webservice",
+        description: dict.Experiences.sideProjectsCard.taskProject.desc,
+        url: "https://github.com/mth-bou/task-management-webservice"
+    },
+    {
+        logo: faTableCells,
+        title: "The Game of Life",
+        description: dict.Experiences.sideProjectsCard.gameOfLifeProject.desc,
+        url: "https://the-game-of-life-mathieu-boucher.vercel.app"
+    },
+    {
+        logo: faRobot,
+        title: "Chatbot",
+        description: dict.Experiences.sideProjectsCard.chatbotProject.desc,
+        url: "https://github.com/mth-bou/chatsonic-chatbot"
+    },
+    {
+        logo: faPersonBiking,
+        title: "vLille Search Project",
+        description: dict.Experiences.sideProjectsCard.vLilleProject.desc,
+        url: "https://github.com/mth-bou/vlille-search"
     },
     {
         logo: faSignal,
