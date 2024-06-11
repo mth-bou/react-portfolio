b6f01a6ee8cf2cce54f6ecd83e58fdfe50c0009a
Mathieu <mathieu.boucher@epsi.fr>
2024-06-11 16:17:06 +0200

    reorder works & missions

--- constants/works.ts
+++ constants/works.ts
@@ -6,34 +6,34 @@ import FaviconHappyvap from '@/public/img/favicon-happyvap.ico';
 
 
 export const WORKS: WorkProps[] = [
-    {
-        image: LogoCR,
-        title: "Clinic Renaissance",
-        role: "Web Developer",
-        date: "2021 - 2023",
-        url: "https://clinic-renaissance.fr"
-    },
-    {
-        image: LikomeIcon,
-        title: "Likome Website",
-        role: "Web Developer",
-        date: "2023",
-        url: "https://likome.fr",
-        freelance: true
-    },
-    {
-        image: LogoOsteocar,
-        title: "Osteocar Website",
-        role: "Web Developer",
-        date: "2022",
-        url: "https://osteocar-osteopathie.fr/",
-        freelance: true
-    },
-    {
-        image: FaviconHappyvap,
-        title: "HappyVap",
-        role: "Web Developer",
-        date: "2020 - 2021",
-        url: "",
-    },
+  {
+    image: LikomeIcon,
+    title: "Likome Website",
+    role: "Web Developer",
+    date: "2024",
+    url: "https://likome.fr",
+    freelance: true
+  },
+  {
+    image: LogoOsteocar,
+    title: "Osteocar Website",
+    role: "Web Developer",
+    date: "2023",
+    url: "https://osteocar-osteopathie.fr/",
+    freelance: true
+  },
+  {
+    image: LogoCR,
+    title: "Clinic Renaissance",
+    role: "Web Developer",
+    date: "2021 - 2023",
+    url: "https://clinic-renaissance.fr"
+  },
+  {
+    image: FaviconHappyvap,
+    title: "HappyVap",
+    role: "Web Developer",
+    date: "2020 - 2021",
+    url: "",
+  },
 ];
