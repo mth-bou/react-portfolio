2d8519702d5d292f6adef099a1d0ced51372bdf0
Mathieu <mathieu.boucher@epsi.fr>
2024-11-16 17:25:15 +0100

    add lyzi experience

--- constants/works.ts
+++ constants/works.ts
@@ -4,16 +4,24 @@ import LogoCR from '@/public/img/logo-cr.jpg';
 import LogoOsteocar from '@/public/img/logo-osteocar.png';
 import FaviconHappyvap from '@/public/img/favicon-happyvap.ico';
 import BerealLogo from '@/public/img/bereal.webp';
+import LyziLogo from '@/public/img/lyzi.svg';
 
 
 export const WORKS: WorkProps[] = [
+  {
+    image: LyziLogo,
+    title: "Lyzi Dashboard WebApp",
+    role: "Web Developer",
+    date: "2024",
+    url: "https://admin.lyzi.io/",
+    freelance: true
+  },
   {
     image: BerealLogo,
     title: "Bereal Shopify App",
     role: "Web Developer",
     date: "2024",
-    freelance: true,
-    inProgress: true
+    freelance: true
   },
   {
     image: LikomeIcon,
