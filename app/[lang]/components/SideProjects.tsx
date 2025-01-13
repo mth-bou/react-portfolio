632ea2e3bd74ee3d175e5d646ac5a1f1e757065e
Mathieu <mathieu.boucher@epsi.fr>
2025-01-13 16:55:56 +0100

    make url property optional and add more side projects

--- app/[lang]/components/SideProjects.tsx
+++ app/[lang]/components/SideProjects.tsx
@@ -6,23 +6,34 @@ export type SideProjectsProps = {
     logo: any;
     title: string;
     description: string;
-    url: string;
+    url?: string;
 }
 export const SideProjects = (props: SideProjectsProps) => {
-    return (
-        <Link
-            href={props.url}
-            target="_blank"
-            className="inline-flex items-center gap-4 hover:bg-accent/50 transition-colors p-2 rounded"
-        >
+    return props.url ? (
+      <Link
+        href={props.url}
+        target="_blank"
+        className="inline-flex items-center gap-4 hover:bg-accent/50 transition-colors p-2 rounded"
+      >
             <span className="bg-accent text-accent-foreground p-3 rounded-sm">
-                <FontAwesomeIcon icon={props.logo} size="lg"/>
+                <FontAwesomeIcon icon={props.logo} size="lg" />
             </span>
 
-            <div>
-                <p className="text-sm md:text-base font-semibold">{props.title}</p>
-                <p className="text-sm md:text-base text-muted-foreground">{props.description}</p>
-            </div>
-        </Link>
+          <div>
+              <p className="text-sm md:text-base font-semibold">{props.title}</p>
+              <p className="text-sm md:text-base text-muted-foreground">{props.description}</p>
+          </div>
+      </Link>
+    ) : (
+      <div className="inline-flex items-center gap-4 bg-muted/20 p-2 rounded">
+            <span className="bg-accent text-accent-foreground p-3 rounded-sm">
+                <FontAwesomeIcon icon={props.logo} size="lg" />
+            </span>
+
+          <div>
+              <p className="text-sm md:text-base font-semibold">{props.title}</p>
+              <p className="text-sm md:text-base text-muted-foreground">{props.description}</p>
+          </div>
+      </div>
     );
 }
--- constants/sideProjects.ts
+++ constants/sideProjects.ts
@@ -1,54 +1,72 @@
 import {
-    faCartShopping,
-    faSignal,
-    faDiagramProject,
-    faTableCells,
-    faRobot,
-    faPersonBiking
+	faSignal,
+	faDiagramProject,
+	faTableCells,
+	faRobot,
+	faPersonBiking,
+	faRotate,
+  faChartSimple,
+	faHexagonNodes,
+	faChartLine,
 } from "@fortawesome/free-solid-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 export const SIDE_PROJECTS = (dict: any) => [
-    {
-        logo: faGithub,
-        title: "Githreads",
-        description: dict.Experiences.sideProjectsCard.githreadsProject.desc,
-        url: "https://githreads-mthb.vercel.app/"
-    },
-    {
-        logo: faCartShopping,
-        title: "Symfony Ecom App",
-        description: dict.Experiences.sideProjectsCard.symfonyProject.desc,
-        url: "https://github.com/mth-bou/ecom"
-    },
-    {
-        logo: faDiagramProject,
-        title: "Task management webservice",
-        description: dict.Experiences.sideProjectsCard.taskProject.desc,
-        url: "https://github.com/mth-bou/task-management-webservice"
-    },
-    {
-        logo: faTableCells,
-        title: "The Game of Life",
-        description: dict.Experiences.sideProjectsCard.gameOfLifeProject.desc,
-        url: "https://the-game-of-life-mathieu-boucher.vercel.app"
-    },
-    {
-        logo: faRobot,
-        title: "Chatbot",
-        description: dict.Experiences.sideProjectsCard.chatbotProject.desc,
-        url: "https://github.com/mth-bou/chatsonic-chatbot"
-    },
-    {
-        logo: faPersonBiking,
-        title: "vLille Search Project",
-        description: dict.Experiences.sideProjectsCard.vLilleProject.desc,
-        url: "https://github.com/mth-bou/vlille-search"
-    },
-    {
-        logo: faSignal,
-        title: "Ping Stability Test",
-        description: dict.Experiences.sideProjectsCard.pingProject.desc,
-        url: "https://github.com/mth-bou/ping-stability-test"
-    }
+	{
+		logo: faGithub,
+		title: dict.Experiences.sideProjectsCard.githreadsProject.title,
+		description: dict.Experiences.sideProjectsCard.githreadsProject.desc,
+		url: "https://githreads-mthb.vercel.app/"
+	},
+	{
+		logo: faRotate,
+		title: dict.Experiences.sideProjectsCard.imagesConverterProject.title,
+		description: dict.Experiences.sideProjectsCard.imagesConverterProject.desc,
+	},
+	{
+		logo: faChartSimple,
+		title: dict.Experiences.sideProjectsCard.binanceTracking.title,
+		description: dict.Experiences.sideProjectsCard.binanceTracking.desc,
+	},
+	{
+		logo: faChartLine,
+		title : dict.Experiences.sideProjectsCard.tradingJournalApi.title,
+		description: dict.Experiences.sideProjectsCard.tradingJournalApi.desc,
+	},
+	{
+		logo: faHexagonNodes,
+		title: dict.Experiences.sideProjectsCard.bookstoreMicroservices.title,
+		description: dict.Experiences.sideProjectsCard.bookstoreMicroservices.desc,
+		url: "https://github.com/mth-bou/nest-bookstore-microservices"
+	},
+	{
+		logo: faDiagramProject,
+		title: dict.Experiences.sideProjectsCard.taskProject.title,
+		description: dict.Experiences.sideProjectsCard.taskProject.desc,
+		url: "https://github.com/mth-bou/task-management-webservice"
+	},
+	{
+		logo: faTableCells,
+		title: dict.Experiences.sideProjectsCard.gameOfLifeProject.title,
+		description: dict.Experiences.sideProjectsCard.gameOfLifeProject.desc,
+		url: "https://the-game-of-life-mathieu-boucher.vercel.app"
+	},
+	{
+		logo: faRobot,
+		title: dict.Experiences.sideProjectsCard.chatbotProject.title,
+		description: dict.Experiences.sideProjectsCard.chatbotProject.desc,
+		url: "https://github.com/mth-bou/chatsonic-chatbot"
+	},
+	{
+		logo: faPersonBiking,
+		title: dict.Experiences.sideProjectsCard.vLilleProject.title,
+		description: dict.Experiences.sideProjectsCard.vLilleProject.desc,
+		url: "https://github.com/mth-bou/vlille-search"
+	},
+	{
+		logo: faSignal,
+		title: dict.Experiences.sideProjectsCard.pingProject.title,
+		description: dict.Experiences.sideProjectsCard.pingProject.desc,
+		url: "https://github.com/mth-bou/ping-stability-test"
+	}
 ];
