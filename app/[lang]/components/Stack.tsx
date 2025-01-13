4767341c231e107102462b9374f1601ebea233e4
Mathieu <mathieu.boucher@epsi.fr>
2025-01-13 17:02:27 +0100

    add nest.js icon to the stack

--- app/[lang]/components/Stack.tsx
+++ app/[lang]/components/Stack.tsx
@@ -4,6 +4,7 @@ import SymfonyIcon from "@/app/[lang]/components/icons/SymfonyIcon";
 import ReactIcon from "@/app/[lang]/components/icons/ReactIcon";
 import NextJsIcon from "@/app/[lang]/components/icons/NextJsIcon";
 import NodeJsIcon from "@/app/[lang]/components/icons/NodeJsIcon";
+import NestIcon from "@/app/[lang]/components/icons/NestIcon";
 import JavascriptIcon from "@/app/[lang]/components/icons/JavascriptIcon";
 import PhpIcon from "@/app/[lang]/components/icons/PhpIcon";
 import TypescriptIcon from "@/app/[lang]/components/icons/TypescriptIcon";
@@ -12,19 +13,20 @@ import SassIcon from "@/app/[lang]/components/icons/SassIcon";
 import HtmlIcon from "@/app/[lang]/components/icons/HtmlIcon";
 import CssIcon from "@/app/[lang]/components/icons/CssIcon";
 import PostgreIcon from "@/app/[lang]/components/icons/PostgreIcon";
-import GraphqlIcon from "@/app/[lang]/components/icons/GraphqlIcon";
 import WebpackIcon from "@/app/[lang]/components/icons/WebpackIcon";
-import SolidityIcon from "@/app/[lang]/components/icons/SolidityIcon";
 import IconList from "@/app/[lang]/components/IconList";
 import MySQLIcon from "@/app/[lang]/components/icons/MySQLIcon";
 import MongoDbIcon from "@/app/[lang]/components/icons/MongoDbIcon";
 import JestIcon from "@/app/[lang]/components/icons/JestIcon";
 import CypressIcon from "@/app/[lang]/components/icons/CypressIcon";
+import GraphqlIcon from "@/app/[lang]/components/icons/GraphqlIcon";
+import SolidityIcon from "@/app/[lang]/components/icons/SolidityIcon";
 
 const Stack = ({ dict }: {dict: any}) => {
 
     const iconList = [
         { IconComponent: NodeJsIcon, content: "NodeJS" },
+        { IconComponent: NestIcon, content: "NestJS" },
         { IconComponent: ReactIcon, content: "React" },
         { IconComponent: NextJsIcon, content: "NextJS" },
         { IconComponent: JavascriptIcon, content: "Javascript" },
